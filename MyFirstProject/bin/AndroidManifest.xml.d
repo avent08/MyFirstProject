c59dab9c77d1ddce7d6db80a039f771989ec6d56
Avent08 <andhika.ventausa@gmail.com>
2012-12-10 14:48:59 +0700

    test commit to jenkins 3

---
+++ MyFirstProject/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProject\bin\AndroidManifest.xml : \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProject\AndroidManifest.xml \
