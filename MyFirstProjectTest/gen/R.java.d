ad6546eff067238d93fdc2b3d56fb876cfbb9387
Avent08 <andhika.ventausa@gmail.com>
2012-12-10 09:16:02 +0700

    insert to jenkins 2

---
+++ MyFirstProjectTest/gen/R.java.d
@@ -0,0 +1,11 @@
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\gen\com\example\myfirstproject\test\R.java \
+ : D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-hdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-ldpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-mdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-xhdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\values\strings.xml \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\res\drawable-hdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\res\drawable-ldpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\res\drawable-mdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\res\drawable-xhdpi\ic_launcher.png \
+D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\AndroidManifest.xml \
