4c95119d99976bd4fdbeca9904da167f2a5804a1
Avent08 <andhika.ventausa@gmail.com>
2012-12-10 17:24:38 +0700

    jenkins android commit 6

--- MyFirstProjectTest/bin/classes/R.java.d
+++
@@ -1,7 +0,0 @@
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\gen\com\example\myfirstproject\test\R.java \
- : D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-hdpi\ic_launcher.png \
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-ldpi\ic_launcher.png \
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-mdpi\ic_launcher.png \
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\drawable-xhdpi\ic_launcher.png \
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\res\values\strings.xml \
-D:\Works\Android Development\Workspace\JUnitTestingProject\MyFirstProjectTest\bin\AndroidManifest.xml \
